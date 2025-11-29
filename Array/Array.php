7f291b0d51e361b9e62a9f9a0ed3b2019a3ca211
unknown <hiipankajvishwakarma@gmail.com>
2025-11-29 16:53:22 +0530

    arrayStore the value used class and object method

--- Array/Array.php
+++ Array/Array.php
@@ -1,7 +1,43 @@
 <!-- <?php
- $fruits = array("Apple ","Bannama","cherry");
-//  echo $fruits[array_rand($fruits)];
-echo "Fruits  " ,$fruits[0];
-echo "",$fruits[1]
-;
- ?> -->
+
+class    student{
+    public $name;
+    public $email =[];
+
+    public function __construct($name ,$email){
+        $this->name = $name;
+        $this->email = $email;
+
+    }
+    //methods to display data
+public function showDateails(){
+    echo "name" . $this->name. "<br>";
+    echo "subject <br>";
+
+    foreach ($this->email as $email){
+        echo " _ ".$email."".$email."";
+    
+}
+}
+}
+
+$subjectsArray =["hiipankajvishwakarma@gmail.com ","satyamkumar@gmail.com","sachin@gmail.com"];
+$subjects = new student("pankaj" ,$subjectsArray);
+
+$subjects->showDateails();
+?> -->
+
+
+<?php
+
+class fruits {
+    public $items = [];
+    public function showFruits(){
+        print_r($this->items);
+}
+}
+$fruitObj =new fruits();
+$fruitObj->items=["apple","banana","mango","grapes"];
+
+$fruitObj->showFruits();
+?>
